3f192ecb2bae64d46a03f1d512f0c35815fcfee8
Will-Bruun <75360110+Will-Bruun@users.noreply.github.com>
2020-12-07 19:28:09 +0100

    Wrong addittion to branch, added right filepath

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,4 +1,22 @@
 package calculator;
 
+import java.util.ArrayList;
+
 public class StringCalculator {
+    public int Add(String nums) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int sum = 0;
+        if (nums == "") {
+            return 0;
+        }
+        String[] split = nums.split(",");
+        for (String num : split) {
+            list.add(Integer.parseInt(num));
+        }
+        for(int i = 0; i < list.size(); i++){
+            int temp = sum;
+            sum = temp + list.get(i);
+        }
+        return sum;
+    }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,4 +1,36 @@
 package calculator;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+
 public class StringCalculatorTest {
+    @Test
+    @DisplayName("Simple test to see if a couple of parameters return right value")
+    void checkReturnTwoNumbers(){
+        StringCalculator calc = new StringCalculator();
+
+        int result = calc.Add("1,2");
+
+        Assertions.assertEquals(3, result, "1 + 2 should be 3");
+    }
+    @Test
+    void checkReturnOneNumber(){
+        StringCalculator calc = new StringCalculator();
+
+        int result = calc.Add("2");
+
+        Assertions.assertEquals(2, result, "2 should be 2");
+    }
+    @Test
+    void checkReturnNullNumbers(){
+        StringCalculator calc = new StringCalculator();
+
+        var result = calc.Add("");
+
+        Assertions.assertEquals(0,result);
+    }
+
 }
