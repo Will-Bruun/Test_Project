a8d15f919d48b3383e09bc0a33ed75332fb591b4
Will-Bruun <75360110+Will-Bruun@users.noreply.github.com>
2020-12-08 11:19:24 +0100

    Refactored Add to use pattern, added tests

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -15,7 +15,7 @@ public class StringCalculatorTest {
     void checkReturnTwoNumbers(){
         StringCalculator calc = new StringCalculator();
 
-        int result = calc.Add("1\n2");
+        int result = calc.Add("1;2");
 
         Assertions.assertEquals(3, result, "1 + 2 should be 3");
     }
@@ -37,7 +37,7 @@ public class StringCalculatorTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"1\n2", "1,2,3", "1,5,6,2"})
+    @ValueSource(strings = {"1\n2", "1,2,3", "1;5;6;2"})
     void checkReturnVariednumbers(String candidate){
         StringCalculator calc = new StringCalculator();
 
